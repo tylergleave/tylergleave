822624558d25eeff5acd544154773495da8b0736
Tyler Gleave <tyler@tylergleave.com>
2023-01-29 16:07:15 -0700

    SvelteKit/Appwite project base commit

---
+++ appwrite.js
@@ -0,0 +1,5 @@
+import { Client, Databases, ID } from 'appwrite';
+
+const client = new Client()
+    .setEndpoint('https://localhost/v1') // Your API Endpoint
+    .setProject('63d6eb09764f09263a4c');               // Your project ID
---
+++ src/routes/portfolio/utah-backcountry-pilots/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Utah Back Country Pilots Association</h1>
+<a href="https://utahbackcountrypilots.org" target="_blank">utahbackcountrypilots.org</a>
